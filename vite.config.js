d3e90cdb12657f214071b86239b1571ed79367c7
Rodolphe E <rodoard@awos.io>
2025-09-28 04:03:52 -0600

    Prepare for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: process.env.PORT || 3000,
+		strictPort: true
+	},
+	preview: {
+		port: process.env.PORT || 3000,
+		strictPort: true
+	}
 });
 
