5b2877ffef0e1d3c57524253500c41b17ccb1aa5
Rodolphe E <rodoard@awos.io>
2025-09-28 04:08:33 -0600

    Convert to static site with @sveltejs/adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		prerender: {
+			handleHttpError: 'warn',
+			entries: ['*']
+		}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
-
